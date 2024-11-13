64867c27de42cea755023ab2bd1b99320ca67c3d
Renan Oliveira <renanaugusto414@gmail.com>
2024-11-13 10:40:45 -0300

    Coloquei a sidbar

---
+++ src/javascript/sidebar.js
@@ -0,0 +1,4 @@
+// Funcionalidade para abrir o botão
+document.getElementById('open_btn').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+});
